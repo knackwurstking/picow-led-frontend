e806b920863519bb5de0b64d6d9b6ec6cc37a0cb
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-30 13:05:21 +0200

    import ui.css and update slot names

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,19 +1,21 @@
 <script>
+    import "ui/dist/ui.css"
+
     // TODO: Setup global websocket handler, store and api handlers,
     //       or maybe use "./+layout.ts" for this
 </script>
 
 <div class="ui-app-bar">
     <span class="ui-app-bar-left">
-        <slot name="left"></slot>
+        <slot name="app-bar-left"></slot>
     </span>
 
     <span class="ui-app-bar-center">
-        <h4><slot name="title"></slot></h4>
+        <h4><slot name="app-bar-title"></slot></h4>
     </span>
 
     <span class="ui-app-bar-right">
-        <slot name="left"></slot>
+        <slot name="app-bar-right"></slot>
     </span>
 </div>
 
