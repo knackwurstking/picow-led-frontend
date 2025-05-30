005970ef81d589878e58fe74cfd24c9e5149965e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-30 11:38:10 +0200

    add /ws to server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:50835/api',
-				changeOrigin: true
-				//rewrite: (path) => path.replace(/^\/api/, ''),
-			}
-		}
-	}
+    plugins: [sveltekit()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:50835/api",
+                changeOrigin: true,
+                //rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+            "/ws": {
+                target: "ws://localhost:50835/ws",
+                changeOrigin: true,
+                ws: true,
+                //rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 });
