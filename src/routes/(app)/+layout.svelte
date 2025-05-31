33f74b5cac46185ab9f06f6b1f41581af8993182
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-31 07:57:08 +0200

    add online indicator

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
     import "ui/dist/ui.css"
+
+    const onlineIndicator_DataState: "offline" | "online" = $state("offline")
 
     // TODO: Setup global websocket handler, store and api handlers,
     //       or maybe use "./+layout.ts" for this
@@ -7,6 +9,8 @@
 
 <div class="ui-app-bar">
     <span class="ui-app-bar-left">
+        <span class="online-indicator" data-state={onlineIndicator_DataState}></span>
+
         <slot name="app-bar-left"></slot>
     </span>
 
@@ -20,3 +24,29 @@
 </div>
 
 <slot></slot>
+
+<style>
+    .online-indicator {
+        --mono: 1;
+        --wght: 650;
+        font-size: 0.75rem;
+    }
+
+    .online-indicator[data-state="online"] {
+        color: green;
+    }
+
+    .online-indicator[data-state="online"]::after {
+        content: "Online";
+        color: green;
+    }
+
+    .online-indicator[data-state="offline"] {
+        color: red;
+    }
+
+    .online-indicator[data-state="offline"]::after {
+        content: "Offline";
+        color: red;
+    }
+</style>
