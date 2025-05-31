1b2c7c6c8b8562fd1341a06f5483a0895e6b101f
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-31 18:46:13 +0200

    add power get/post (api)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,6 @@
+export const PowerStateOFF = 0;
+export const PowerStateON = 1;
+
 export const devices = {
     async get(): Promise<Devices> {
         const r = await fetch(`/api/devices`);
@@ -7,5 +10,27 @@ export const devices = {
         }
 
         return await r.json();
+    },
+};
+
+export const power = {
+    async get(addr: string): Promise<PowerState> {
+        const r = await fetch(`/api/devices/${addr}/power`);
+
+        if (!r.ok) {
+            throw new Error(r.statusText);
+        }
+
+        return await r.json();
+    },
+
+    async post(addr: string, state: PowerState) {
+        const r = await fetch(`/api/devices/${addr}/power?state=${state}`, {
+            method: "POST",
+        });
+
+        if (!r.ok) {
+            throw new Error(r.statusText);
+        }
     },
 };
