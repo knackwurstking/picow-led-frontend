1ab3aad6b7e60784ab79b78a6fe430b4e8067913
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-31 09:19:21 +0200

    add device item component (wip)

---
+++ src/lib/components/DeviceItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let {
+        addr, name, color, pins, active_color, power
+    }: Device = $props()
+</script>
+
+<li>
+    <!-- TODO: Render device list item here -->
+</li>
