3956bc738819f8c72296cab080a71331c7b23f18
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-31 09:03:29 +0200

    log ws message events

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        // TODO: Fetch devices from /api and render items
+
+        // TODO: Handle WebSocket message events ("devices", "device")
+        window.ws.events.addListener("message", async (data) => {
+            switch (data.type) {
+                case "devices":
+                    {
+                        console.debug(`ws "devices" event:`, data.data)
+                        // ...
+                    }
+                    break
+
+                case "device":
+                    {
+                        console.debug(`ws "device" event:`, data.data)
+                        // ...
+                    }
+                    break
+            }
+        })
+    })
 </script>
 
 <svelte:head>
     <title>PicoW LED | Devices</title>
 </svelte:head>
+
+<ul id="deviceList"></ul>
