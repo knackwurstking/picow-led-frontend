4564b20b0c86b40bf2b40a634cad79c4f7018bf7
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-31 08:27:34 +0200

    add data types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,47 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
+
+    type Device = {
+        addr: string;
+        name: string;
+        color: number[];
+        pins: number[];
+        active_color: number[];
+        power: 0 | 1;
+    };
+
+    type Devices = Device[];
+
+    type Color = {
+        id: number;
+        r: number;
+        g: number;
+        b: number;
+    };
+
+    type Colors = Color[];
+
+    type WSMessageData =
+        | {
+              type: "devices";
+              data: Devices;
+          }
+        | {
+              type: "device";
+              data: Device;
+          }
+        | {
+              type: "colors";
+              data: Colors;
+          };
 }
 
 export {};
