58c6c6d9d5eb6863e07c6b5571827c548d1a8091
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-02 22:59:52 +0200

    vite-pwa: add basic configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), SvelteKitPWA()],
     server: {
         proxy: {
             "/api": {
