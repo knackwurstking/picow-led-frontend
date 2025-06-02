f5dd3018f0b8540d198eb12d9155af47ae505e2b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-03 00:08:44 +0200

    remove vite-plugin-pwa for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [sveltekit(), VitePWA()],
+    plugins: [sveltekit()],
     server: {
         proxy: {
             "/api": {
