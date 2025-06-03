000705e861a1bdb793097ca40a519340d3f4476a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-03 14:47:26 +0200

    Add todos

--- src/routes/device/+page.svelte
+++ src/routes/device/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
     import { utils } from "$lib"
+    import { onMount } from "svelte";
 
     const queryAddr = utils.urlQueryParam("addr")
+
+    onMount(async () => {
+        // TODO: Fetch colors from the api
+    })
 </script>
 
 <svelte:head>
     <title>PicoW LED | {queryAddr}</title>
 </svelte:head>
+
+<!-- TODO: Continue here, page building -->
