547ebc6375b4c691e96ce4ff84447baa0721e6ea
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-03 18:44:03 +0200

    Update device page (wip)

--- src/routes/device/+page.svelte
+++ src/routes/device/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-    import { utils } from "$lib"
+    import { api, utils } from "$lib"
     import { onMount } from "svelte";
 
     const queryAddr = utils.urlQueryParam("addr")
 
+    let device = $state<Device | undefined>(undefined)
+
+    async function powerOFF() {
+        // TODO: ...
+    }
+
+    async function powerON() {
+        // TODO: ...
+    }
+
     onMount(async () => {
+        // Fetch device from api
+        device = await api.device.get(queryAddr)
+
         // TODO: Fetch colors from the api
     })
 </script>
@@ -13,4 +26,50 @@
     <title>PicoW LED | {queryAddr}</title>
 </svelte:head>
 
-<!-- TODO: Continue here, page building -->
+<div class="ui-flex column gap">
+    <span class="power ui-flex gap justify-between align-center">
+        <span>Power</span>
+
+        <span>
+            <button class="off" data-ui-color="destructive" onclick={powerOFF}>
+                OFF
+            </button>
+
+            <button class="on" onclick={powerON}>ON</button>
+        </span>
+    </span>
+
+    <h2>Color Storage</h2>
+
+    {#if device && device.pins.length > 3}
+        <div
+            class="range-sliders ui-flex column gap nowrap"
+            style="display: none"
+        >
+            {#each device.pins.slice(3) as pin}
+            {/each}
+        </div>
+    {/if}
+
+    <br />
+
+    <div class="color-storage-container ui-flex row gap wrap">
+    </div>
+
+    <button class="new-color ui-flex-item">
+        New Color
+
+        <input type="color" />
+    </button>
+</div>
+
+<style>
+    button.new-color input[type="color"] {
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
