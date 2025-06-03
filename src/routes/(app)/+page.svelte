c53f3ba2dd0b1b1473344a14b786b220a11595b7
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-04 00:18:36 +0200

    Merge layout into page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
+    import "ui/dist/ui.css"
+
     import { onMount } from "svelte"
+    import * as ui from "ui"
 
     import { api } from "$lib";
 
     import DeviceItem from "$lib/components/DeviceItem.svelte";
+    import OnlineIndicator from "$lib/components/OnlineIndicator.svelte";
+
+    const ws = new ui.WS<WSMessageData>("/ws", true)
 
     let devices = $state<Devices>([])
+    let onlineIndicator_DataState = $state<"offline" | "online">("offline")
 
     onMount(async () => {
         // Fetch devices from /api and render items
         devices = await api.devices.get()
 
+        await ws.connect()
+
+        console.debug("Adding all WebSocket event listeners")
+
+        ws.events.addListener("open", () => {
+            console.debug("ws open...")
+            onlineIndicator_DataState = "online"
+        })
+
+        ws.events.addListener("close", () => {
+            console.debug("ws close...")
+            onlineIndicator_DataState = "offline"
+        })
+
         // Handle WebSocket message events ("devices", "device")
-        window.ws.events.addListener("message", async (data) => {
+        ws.events.addListener("message", async (data) => {
             switch (data.type) {
                 case "devices":
                     {
@@ -44,8 +65,28 @@
     <title>PicoW LED | Devices</title>
 </svelte:head>
 
-<ul>
-    {#each devices as device}
-        <DeviceItem {...device} />
-    {/each}
-</ul>
+<main class="ui-container">
+    <ul>
+        {#each devices as device}
+            <DeviceItem {...device} />
+        {/each}
+    </ul>
+</main>
+
+<div class="ui-app-bar" data-ui-position="top">
+    <span class="ui-app-bar-left">
+        <OnlineIndicator state={onlineIndicator_DataState} />
+    </span>
+
+    <span class="ui-app-bar-center">
+        <h4>Devices</h4>
+    </span>
+
+    <span class="ui-app-bar-right"></span>
+</div>
+
+<style>
+    main {
+        padding-top: calc(var(--ui-app-bar-height) + var(--ui-spacing));
+    }
+</style>
