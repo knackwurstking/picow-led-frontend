a4c1327e1b053ebf793a86827992fa48bbaf351d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-03 14:10:18 +0200

    Add todos (snippets)

---
+++ src/routes/(app)/device/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    // ...
+</script>
+
+{#snippet appBarTitle()}
+    <!-- TODO: Device name or address here -->
+{/snippet}
+
+{#snippet appBarLeft()}
+    <!-- TODO: Left AppBar items here -->
+{/snippet}
+
+{#snippet appBarRight()}
+    <!-- TODO: Right AppBar items here -->
+{/snippet}
