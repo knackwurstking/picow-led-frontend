6c63607b60174d6f6093de7675614e42c2511cdc
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-06 05:03:16 +0200

    Add toast message and dismiss toast

--- src/lib/components/Toasts.svelte
+++ src/lib/components/Toasts.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     // NOTE: https://svelte.dev/playground/0091c8b604b74ed88bb7b6d174504f50?version=3.35.0
 
-    import { toasts } from "../store/toasts";
+    import { toasts, dismissToast } from "../store/toasts";
 
     import Toast from "./Toast.svelte";
 </script>
@@ -10,12 +10,13 @@
     {#each $toasts as toast}
         <Toast
             type={toast.type}
-            message={toast.message}
             timeout={toast.timeout}
             ondismiss={async () => {
-                // TODO: Remove from $toasts
+                dismissToast(toast.id);
             }}
-        />
+        >
+            {toast.message}
+        </Toast>
     {/each}
 </section>
 
