155cf2fdefea32e08f6401fd33c02d0c29601e72
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-06 23:21:39 +0200

    Add a toast if somethings throws an error in api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,31 +1,150 @@
+import { addToast } from "./store/toasts";
+
 export const PowerStateOFF = 0;
 export const PowerStateON = 1;
 
 export const devices = {
     async GET(): Promise<Devices> {
-        const r = await fetch(`/api/devices`);
+        const url = `/api/devices`;
 
-        if (!r.ok) {
-            throw new Error(r.statusText);
-        }
-
-        return await r.json();
-    },
-
-    addr: {
-        async GET(addr: string): Promise<Device> {
-            const r = await fetch(`/api/devices/${addr}`);
+        try {
+            const r = await fetch(url);
 
             if (!r.ok) {
                 throw new Error(r.statusText);
             }
 
             return await r.json();
+        } catch (err) {
+            addToast("error", `Fetch failed: ${url}: ${err}`);
+            throw err;
+        }
+    },
+
+    addr: {
+        async GET(addr: string): Promise<Device> {
+            const url = `/api/devices/${addr}`;
+
+            try {
+                const r = await fetch(url);
+
+                if (!r.ok) {
+                    throw new Error(r.statusText);
+                }
+
+                return await r.json();
+            } catch (err) {
+                addToast("error", `Fetch failed: ${url}: ${err}`);
+                throw err;
+            }
         },
 
         color: {
             async POST(addr: string, color: number[]): Promise<void> {
-                const r = await fetch(`/api/devices/${addr}/color`, {
+                const url = `/api/devices/${addr}/color`;
+
+                try {
+                    const r = await fetch(url, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(color),
+                    });
+
+                    if (!r.ok) {
+                        throw new Error(r.statusText);
+                    }
+                } catch (err) {
+                    addToast("error", `Fetch failed: ${url}: ${err}`);
+                    throw err;
+                }
+            },
+        },
+
+        power: {
+            async GET(addr: string): Promise<PowerState> {
+                const url = `/api/devices/${addr}/power`;
+
+                try {
+                    const r = await fetch(url);
+
+                    if (!r.ok) {
+                        throw new Error(r.statusText);
+                    }
+
+                    return await r.json();
+                } catch (err) {
+                    addToast("error", `Fetch failed: ${url}: ${err}`);
+                    throw err;
+                }
+            },
+
+            async POST(addr: string, state: PowerState) {
+                const url = `/api/devices/${addr}/power?state=${state}`;
+
+                try {
+                    const r = await fetch(url, {
+                        method: "POST",
+                    });
+
+                    if (!r.ok) {
+                        throw new Error(r.statusText);
+                    }
+                } catch (err) {
+                    addToast("error", `Fetch failed: ${url}: ${err}`);
+                    throw err;
+                }
+            },
+        },
+    },
+};
+
+export const colors = {
+    async GET(): Promise<Colors> {
+        const url = `/api/colors`;
+
+        try {
+            const r = await fetch(url);
+
+            if (!r.ok) {
+                throw new Error(r.statusText);
+            }
+
+            return await r.json();
+        } catch (err) {
+            addToast("error", `Fetch failed: ${url}: ${err}`);
+            throw err;
+        }
+    },
+
+    async POST(colors: Colors): Promise<void> {
+        const url = `/api/colors`;
+
+        try {
+            const r = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(colors),
+            });
+
+            if (!r.ok) {
+                throw new Error(r.statusText);
+            }
+        } catch (err) {
+            addToast("error", `Fetch failed: ${url}: ${err}`);
+            throw err;
+        }
+    },
+
+    id: {
+        async POST(id: number, color: Color): Promise<void> {
+            const url = `/api/colors/${id}`;
+
+            try {
+                const r = await fetch(url, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -36,73 +155,9 @@ export const devices = {
                 if (!r.ok) {
                     throw new Error(r.statusText);
                 }
-            },
-        },
-
-        power: {
-            async GET(addr: string): Promise<PowerState> {
-                const r = await fetch(`/api/devices/${addr}/power`);
-
-                if (!r.ok) {
-                    throw new Error(r.statusText);
-                }
-
-                return await r.json();
-            },
-
-            async POST(addr: string, state: PowerState) {
-                const r = await fetch(
-                    `/api/devices/${addr}/power?state=${state}`,
-                    {
-                        method: "POST",
-                    },
-                );
-
-                if (!r.ok) {
-                    throw new Error(r.statusText);
-                }
-            },
-        },
-    },
-};
-
-export const colors = {
-    async GET(): Promise<Colors> {
-        const r = await fetch(`/api/colors`);
-
-        if (!r.ok) {
-            throw new Error(r.statusText);
-        }
-
-        return await r.json();
-    },
-
-    async POST(colors: Colors): Promise<void> {
-        const r = await fetch(`/api/colors`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(colors),
-        });
-
-        if (!r.ok) {
-            throw new Error(r.statusText);
-        }
-    },
-
-    id: {
-        async POST(id: number, color: Color): Promise<void> {
-            const r = await fetch(`/api/colors/${id}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(color),
-            });
-
-            if (!r.ok) {
-                throw new Error(r.statusText);
+            } catch (err) {
+                addToast("error", `Fetch failed: ${url}: ${err}`);
+                throw err;
             }
         },
     },
