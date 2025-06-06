60eb70eb3010c053d89e714a22faa25da8c7152d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-06 05:02:47 +0200

    Add TODOs

--- src/lib/store/toasts.ts
+++ src/lib/store/toasts.ts
@@ -3,9 +3,22 @@ import { writable } from "svelte/store";
 export type ToastDataTypes = "info" | "warning" | "error";
 
 export interface ToastData {
+    id: number;
     type: ToastDataTypes;
     message: string;
     timeout: number;
 }
 
 export const toasts = writable<ToastData[]>([]);
+
+export function addToast(
+    type: ToastDataTypes,
+    message: string,
+    timeout: number = 0,
+): void {
+    // TODO: ...
+}
+
+export function dismissToast(id: number): void {
+    // TODO: ...
+}
