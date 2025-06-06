a51a3977af3c5e981dabd7fd64e799568ca5ac37
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-07 00:13:01 +0200

    Comment out the testing part

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,29 +8,28 @@
 
     let { children } = $props();
 
-    onMount(async () => {
-        let data: Partial<ToastData>[] = [
-            { type: "info", message: "Info test message" },
-            { type: "warning", message: "Warning test message" },
-            { type: "error", message: "Warning test message" },
-            {
-                type: "info",
-                message:
-                    "A very long info test message, I want to test the line break here.",
-            },
-        ];
-
-        const timeoutHandler = (d: Partial<ToastData> | undefined) => {
-            if (!d) {
-                return;
-            }
-
-            addToast(d.type!, d.message!, 5000);
-            setTimeout(() => timeoutHandler(data.shift()), 1000);
-        };
-
-        setTimeout(() => timeoutHandler(data.shift()), 1000);
-    });
+    // NOTE: This part is just for testing toasts
+    //
+    //onMount(async () => {
+    //    let data: Partial<ToastData>[] = [
+    //        { type: "info", message: "Info test message" },
+    //        { type: "warning", message: "Warning test message" },
+    //        { type: "error", message: "Warning test message" },
+    //        {
+    //            type: "info",
+    //            message:
+    //                "A very long info test message, I want to test the line break here.",
+    //        },
+    //    ];
+    //    const timeoutHandler = (d: Partial<ToastData> | undefined) => {
+    //        if (!d) {
+    //            return;
+    //        }
+    //        addToast(d.type!, d.message!, 5000);
+    //        setTimeout(() => timeoutHandler(data.shift()), 1000);
+    //    };
+    //    setTimeout(() => timeoutHandler(data.shift()), 1000);
+    //});
 </script>
 
 {@render children()}
