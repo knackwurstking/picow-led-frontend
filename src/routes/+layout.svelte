20931c145b19925c3b97b866cc755ca631d7f3f0
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-06 23:06:12 +0200

    Render Toasts component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
     import "./bootstrap-icons.css";
 
+    import Toasts from "$lib/components/Toasts.svelte";
+
     let { children } = $props();
 </script>
 
 {@render children()}
+
+<Toasts />
