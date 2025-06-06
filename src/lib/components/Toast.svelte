70e851269db3be45311c8e485425f916710a2df2
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-06 05:02:35 +0200

    Render toast and style a bit

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import type { Snippet } from "svelte";
     import type { ToastDataTypes } from "../store/toasts";
 
     let {
@@ -6,10 +7,44 @@
         message,
         timeout = 0, // If number is 0 or lower, timeout handler is ignored
         ondismiss,
+        children,
     }: {
         type: ToastDataTypes;
         message: string;
         timeout?: number;
         ondismiss: () => void | Promise<void>;
+        children: Snippet<[]>;
     } = $props();
 </script>
+
+{#if type === "info"}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div class="toast toast-info ui-backdrop ui-border" onclick={ondismiss}>
+        {@render children()}
+    </div>
+{:else if type === "warning"}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div class="toast toast-warning ui-backdrop ui-border" onclick={ondismiss}>
+        {@render children()}
+    </div>
+{:else if type === "error"}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div class="toast toast-error ui-backdrop ui-border" onclick={ondismiss}>
+        {@render children()}
+    </div>
+{/if}
+
+<style>
+    .toast-warning {
+        color: var(--ui-warning-text);
+        background-color: var(--ui-warning);
+    }
+
+    .toast-error {
+        color: var(--ui-error-text);
+        background-color: var(--ui-error);
+    }
+</style>
